1155eeb119402772c86e077d1baf2f3d5e8bca52
Hangning Li <lihangningnnnn@gmail.com>
2023-03-20 14:26:53 -0400

    update background task

--- ios/BackgroundTaskManager.swift
+++ ios/BackgroundTaskManager.swift
@@ -24,32 +24,19 @@ class BackgroundTaskManager : NSObject {
   
   @objc
   func startReading(){
-    scheduleBackgroundProcessing()
-    print("hello")
-  }
-  
-  
-  // Register the task
-  func registerBackgroundTask() {
-
+    if #available(iOS 14.0, *) {
+      Logger().info("reading started")
+    }
   }
   
   @available(iOS 13.0, *)
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-      // Enable background fetch
-    
       // Register background task
-    
-      BGTaskScheduler.shared.register(forTaskWithIdentifier: backgroundTaskIdentifier, using: nil) { task in
-        // Handle the task
-        self.handleBackgroundTask(task: task as! BGProcessingTask)
-        if #available(iOS 14.0, *) {
-          Logger().info("[BGTASK] Perform bg processing \(self.backgroundTaskIdentifier)")
-        }
+      registerBackgroundTask()
         
-//        task.setTaskCompleted(success: true)
+      if #available(iOS 14.0, *) {
+        Logger().info("[BGTASK] Perform bg processing \(self.backgroundTaskIdentifier)")
       }
-      self.scheduleBackgroundProcessing()
     
       return true
   }
@@ -71,78 +58,85 @@ class BackgroundTaskManager : NSObject {
   }
   
   @available(iOS 13.0, *)
-  func handleBackgroundTask(task: BGProcessingTask) {
+  func registerBackgroundTask() {
+    BGTaskScheduler.shared.register(forTaskWithIdentifier: backgroundTaskIdentifier, using: nil) { task in
       // Handle the task
       // Create a motion manager
       // Set up the accelerometer
-      motionManager.accelerometerUpdateInterval = 0.1 // Update interval in seconds
-      motionManager.gyroUpdateInterval = 0.1
+      self.motionManager.accelerometerUpdateInterval = 0.1 // Update interval in seconds
+      self.motionManager.gyroUpdateInterval = 0.1
       
-//      if motionManager.isAccelerometerAvailable {
-        guard let accelerometerTask = task as? BGProcessingTask else {
-            return
+      //      if motionManager.isAccelerometerAvailable {
+      guard let accelerometerTask = task as? BGProcessingTask else {
+        return
+      }
+      // Start the accelerometer updates
+      self.motionManager.startAccelerometerUpdates(to: .main) { (data, error) in
+        //            guard let accelerometerData = data else {
+        //                return
+        //            }
+        
+        // Do something with the accelerometer data, such as send it to a server
+        if let acceleration = data?.acceleration {
+          // Do something with the accelerometer data
+          print("Acceleration x: \(acceleration.x), y: \(acceleration.y), z: \(acceleration.z)")
         }
-        // Start the accelerometer updates
-        motionManager.startAccelerometerUpdates(to: .main) { (data, error) in
-//            guard let accelerometerData = data else {
-//                return
-//            }
-            
-            // Do something with the accelerometer data, such as send it to a server
-          if let acceleration = data?.acceleration {
-            // Do something with the accelerometer data
-            print("Acceleration x: \(acceleration.x), y: \(acceleration.y), z: \(acceleration.z)")
-          }
-          // Mark the task as complete
-          accelerometerTask.setTaskCompleted(success: true)
-        }
-//      }
-    
-//
-//      if motionManager.isGyroAvailable {
-//        guard let gyroscopeTask = task as? BGProcessingTask else {
-//            return
-//        }
-//        motionManager.startGyroUpdates(to: .main) { (data, error) in
-////            guard let gyroscopeData = data else {
-////                return
-////            }
-//
-//            // Do something with the gyroscope data, such as send it to a server
-//          if let rotationRate = data?.rotationRate {
-//            // Do something with the gyroscope data
-//            print("Rotation rate x: \(rotationRate.x), y: \(rotationRate.y), z: \(rotationRate.z)")
-//          }
-//          // Mark the task as complete
-//          gyroscopeTask.setTaskCompleted(success: true)
-//        }
-//      }
-//    task.setTaskCompleted(success: true)
+        // Mark the task as complete
+        accelerometerTask.setTaskCompleted(success: true)
+      }
+      print("what??")
+      //      }
+      
+      //
+      //      if motionManager.isGyroAvailable {
+      //        guard let gyroscopeTask = task as? BGProcessingTask else {
+      //            return
+      //        }
+      //        motionManager.startGyroUpdates(to: .main) { (data, error) in
+      ////            guard let gyroscopeData = data else {
+      ////                return
+      ////            }
+      //
+      //            // Do something with the gyroscope data, such as send it to a server
+      //          if let rotationRate = data?.rotationRate {
+      //            // Do something with the gyroscope data
+      //            print("Rotation rate x: \(rotationRate.x), y: \(rotationRate.y), z: \(rotationRate.z)")
+      //          }
+      //          // Mark the task as complete
+      //          gyroscopeTask.setTaskCompleted(success: true)
+      //        }
+      //      }
+      //    task.setTaskCompleted(success: true)
+      self.scheduleBackgroundProcessing()
+    }
     
   }
 
   @available(iOS 13.0, *)
   func scheduleBackgroundProcessing() {
-     
-    let taskRequest = BGProcessingTaskRequest(identifier: self.backgroundTaskIdentifier)
-    taskRequest.requiresNetworkConnectivity = false
-    taskRequest.requiresExternalPower = false
-    taskRequest.earliestBeginDate = Date(timeIntervalSinceNow: 60 * 2)
-    
     // Read sensor data for 100 seconds
 //    DispatchQueue.main.asyncAfter(deadline: .now() + 100) {
 //       // Stop motion updates and complete the background task
 //      self.motionManager.stopAccelerometerUpdates()
 //      self.motionManager.stopGyroUpdates()
-//      taskRequest.setTaskCompleted(success: true)
+//      task.setTaskCompleted(success: true)
 //    }
-    
+//
+//    task.expirationHandler = {
+//      self.motionManager.stopAccelerometerUpdates()
+//      self.motionManager.stopGyroUpdates()
+//      task.setTaskCompleted(success: false)
+//    }
+//
+//    task.setTaskCompleted(success: true)
+    let task = BGProcessingTaskRequest(identifier: backgroundTaskIdentifier)
+    task.requiresNetworkConnectivity = true // Set to true if your task requires network connectivity
+    task.requiresExternalPower = true  // Set to true if your task requires external power
     do {
-        try BGTaskScheduler.shared.submit(taskRequest)
+      try BGTaskScheduler.shared.submit(task)
     } catch {
-        print("Error submitting background task: \(error.localizedDescription)")
+      print("Unable to submit task: \(error.localizedDescription)")
     }
   }
-
   
 }
